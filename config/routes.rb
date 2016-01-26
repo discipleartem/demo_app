7340541d826ac1282d70c1cb6e4d837d1707c06c
Tomas <magyc@rambler.ru>
2016-01-26 17:51:42 +0200

    fix route view and readme

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+  root 'microposts#index'
   resources :microposts
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
